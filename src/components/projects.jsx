b2da26a08a6ccf3786d38eb93251b041c8f101c8
Ruth Wanjiru <ruthmwaniki960@gmail.com>
2025-04-28 22:52:08 +0300

    update

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -2,9 +2,27 @@
 export default function Projects(){
 
     return(
-        <div>
-            <h1>Innovative Projects for Sustainable Growth</h1>
-            <p>Transforming Communities Through Sustainable Practices</p>
+        <div
+            style={{
+                backgroundImage: "linear-gradient(to right,rgb(231, 158, 231),rgb(146, 11, 117))",
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: '40vh',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                color: 'white',
+                textAlign: 'center',
+                padding: '20px',
+            }}
+        >
+            <h1 style={{ fontSize: '3rem', fontWeight: 'bold', marginBottom: '1rem' }}>
+            Innovative Projects for Sustainable Growth
+            </h1>
+            <p style={{ fontSize: '1.2rem', marginBottom: '2rem' }}>
+                Transforming Communities Through Sustainable Practices </p>
         </div>
+
     )
 }
