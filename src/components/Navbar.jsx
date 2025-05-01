3af10846c1bc490584cdba7e0b18eeae8dca83a8
Ruth Wanjiru <ruthmwaniki960@gmail.com>
2025-05-01 21:48:37 +0300

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+
 import { NavLink } from 'react-router-dom';
-import { Menu, X } from 'lucide-react';
+
 
 export default function Navbar({ links, title }) {
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection';
 import RecentProjects from '../components/recentProjects';
+import Feedback from '../components/Feedback'
 
 function HomePage() {
   return (
     <div>
       <HeroSection />
       <RecentProjects/>
+      <Feedback />
     </div>
   )
 }
