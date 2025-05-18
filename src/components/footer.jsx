c0bb1c36a4470b349230a4d3023a650f0b62fcb1
Ruth960 <ruthmwaniki960@gmail.com>
2025-05-18 17:52:33 +0300

    Update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,19 +1,19 @@
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-4">
+    <footer className="bg-none text-emerald-950 py-4">
       <div className="container mx-auto text-center">
         <p className="text-sm">
           &copy; {new Date().getFullYear()} Bahari Mezani. All rights reserved.
         </p>
         <div className="flex justify-center space-x-4 mt-2">
-          <a href="#" className="text-gray-400 hover:text-white">
+          <a href="#" className="text-gray-900 hover:text-white">
             Facebook
           </a>
-          <a href="https://www.linkedin.com/company/mesuct/about/" className="text-gray-400 hover:text-white">
+          <a href="https://www.linkedin.com/company/mesuct/about/" className="text-gray-900 hover:text-white">
             Linkedln
           </a>
-          <a href="#" className="text-gray-400 hover:text-white">
+          <a href="#" className="text-gray-900 hover:text-white">
             Instagram
           </a>
         </div>
