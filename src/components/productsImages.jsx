78a6aab142d94f9fe9ae73d3fd2689cb79d33385
Ruth960 <ruthmwaniki960@gmail.com>
2025-07-01 20:50:26 +0300

    updates

--- src/components/productsImages.jsx
+++ src/components/productsImages.jsx
@@ -1,14 +1,33 @@
+export default function ProductsImages() {
+  return (
+    <div className="flex flex-wrap justify-center mt-20 gap-1 p-4">
+     
+      <div className="relative w-58 h-58">
+        <img src="./images/Tilapia.jpeg" alt="Tilapia" className="w-full h-full object-cover" />
+        <p className="absolute top-0 left-0 bg-black bg-opacity-50 text-white p-1 text-sm">Tilapia</p>
+      </div>
+      <div className="relative w-58 h-58">
+        <img src="./images/Catfish.jpeg" alt="Catfish" className="w-full h-full object-cover" />
+        <p className="absolute top-0 left-0 bg-black bg-opacity-50 text-white p-1 text-sm">Catfish</p>
+      </div>
 
-export default function ProductsImages (){
+      <div className="relative w-58 h-58">
+        <img src="./images/Seabass.jpeg" alt="Seabass" className="w-full h-full object-cover" />
+        <p className="absolute top-0 left-0 bg-black bg-opacity-50 text-white p-1 text-sm">Seabass</p>
+      </div>
+      <div className="relative w-58 h-58">
+        <img src="./images/Tuna.jpeg" alt="Tuna" className="w-full h-full object-cover" />
+        <p className="absolute top-0 left-0 bg-black bg-opacity-50 text-white p-1 text-sm">Tuna</p>
+      </div>
+      <div className="relative w-58 h-58">
+        <img src="./images/Salmon.jpeg" alt="Salmon" className="w-full h-full object-cover" />
+        <p className="absolute top-0 left-0 bg-black bg-opacity-50 text-white p-1 text-sm">Salmon</p>
+      </div>
 
-    return (
-        <div className="flex flex-wrap justify-center mt-20 gap-1 p-4">
-            <img src="./images/Tilapia.jpeg" alt="Product 1" className="w-58 justify-center h-58 object-cover" />
-            <img src="./images/Catfish.jpeg" alt="Product 2" className="w-58 h-58 justify-center  object-cover" />
-            <img src="./images/Seabass.jpeg" alt="Product 3" className="w-58 h-58 justify-center  object-cover" />
-            <img src="./images/Tuna.jpeg" alt="Product 3" className="w-58 h-58 justify-center  object-cover" />
-            <img src="./images/Salmon.jpeg" alt="Product 3" className="w-58 h-58 justify-center  object-cover" />
-            <img src="./images/Dorado.jpeg" alt="Product 3" className="w-58 h-58 justify-center  object-cover" />
-        </div>
-    );
+      <div className="relative w-58 h-58">
+        <img src="./images/Dorado.jpeg" alt="Dorado" className="w-full h-full object-cover" />
+        <p className="absolute top-0 left-0 bg-black bg-opacity-50 text-white p-1 text-sm">Dorado</p>
+      </div>
+    </div>
+  );
 }
